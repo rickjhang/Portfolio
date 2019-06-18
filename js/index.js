663c34c3b1fd3d0027ecfed4d53b3ae2835fc37a
rickjhang <whatup110119@gmail.com>
2019-06-18 15:45:02 +0800

    0618第一版完成

--- js/index.js
+++ js/index.js
@@ -6,5 +6,23 @@ $(document).ready(function(){
             $('html,body').animate({ scrollTop: $('.ftTitle').offset().top }, 800);
             $("#menu-contorl").prop("checked",false);
         }
-    }); 
+    });
+    
+
+    if($(window).width()<768){
+        setInterval(function(){
+            $(".iaImg img").css({"opacity":"0","transition":"1.5s"});
+            setTimeout(function(){
+                $(".iaImg img").css({"opacity":"1","transition":"1.5s"});
+            },5000)
+        },9000)
+    }else{
+        $(".iaImg img").mouseover(function(){
+            $(this).css({"opacity":"0","transition":"0.5s"})
+        })
+        $(".iaImg img").mouseout(function(){
+            $(this).css({"opacity":"1","transition":"0.5s"})
+        })
+    }
+
 })
